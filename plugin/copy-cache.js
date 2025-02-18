e6d0217df3ad43f7921901f23e91e6ab7d890555
Maxim Belov <belov1988@gmail.com>
2025-02-18 11:39:15 +0200

    add local libs @see reason https://tanersener.medium.com/saying-goodbye-to-ffmpegkit-33ae939767e1

---
+++ plugin/copy-cache.js
@@ -0,0 +1,15 @@
+const fs = require('node:fs');
+const os = require('node:os');
+
+// const userHomeDir = os.homedir();
+// const src = `${__dirname}/libs/arthenica`;
+// const dest = `${userHomeDir}/.m2/repository/com/arthenica`;
+// if (!fs.existsSync(dest)) {
+//   fs.cpSync(src, dest, {recursive: true});
+// }
+
+// const dest = `${userHomeDir}/.gradle/caches/modules-2/files-2.1/com.arthenica`;
+// const src = `${__dirname}/libs/com.arthenica`;
+// if (!fs.existsSync(dest)) {
+//   fs.cpSync(src, dest, {recursive: true});
+// }
